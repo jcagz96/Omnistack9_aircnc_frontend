a81530b6e673042bbc06e79378cc507016c77d31
jcagz96 <jcagz@iscte-iul.pt>
2019-10-02 19:58:06 +0100

    added pages & api services

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import { BroweserRouter, Switch, Route } from 'react-router-dom';
+
+import Login from './pages/Login/index';
+import Dashboard from './pages/Dashboard/index';
+import New from './pages/New/index';
+
+export default function Routes() {
+    return (
+
+    );
+}
